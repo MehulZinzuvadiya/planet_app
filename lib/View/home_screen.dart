1639d6dd05e59d0b16a3d9d61ffe12a6e701b5a8
MehulZinzuvadiya <mehulzinzuvadiya098@gmail.com>
2023-05-05 09:07:33 +0530

    Initial commit

---
+++ lib/View/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Treva",
+          style: GoogleFonts.poppins(fontSize: 22),
+        ),
+        leading: Icon(Icons.menu),
+      ),
+    ));
+  }
+}
